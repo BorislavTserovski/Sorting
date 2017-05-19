4ca84586a959ea09e2347ee737384d5cd7b31644
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-05-19 13:52:07 +0300

    Bubble+Insertion

---
+++ Algorithms/Bubble/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] inputLine = new[] { 1, 7, 6, 9, 87, 54, 32, 11, 7, 8 };
+            bool isSorted = false;
+            while (!isSorted)
+            {
+                isSorted = true;
+                for (int i = 0; i < inputLine.Length - 1; i++)
+                {
+                    if (inputLine[i] > inputLine[i + 1])
+                    {
+                        var temp = inputLine[i + 1];
+                        inputLine[i + 1] = inputLine[i];
+                        inputLine[i] = temp;
+                        isSorted = false;
+                    }
+                }
+            }
+
+            foreach (var num in inputLine)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+    }
+}
---
+++ Algorithms/Insertion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] input = new[] { 1, 32, 34, 42, 2, 3, 5, 65, 74, 45, 32 };
+            Console.WriteLine("Unsorted - {0}", string.Join(", ", input));
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                int current = input[i];
+                int j = i - 1;
+                for (; j >= 0 && current < input[j]; j--)
+                {
+                    input[j + 1] = input[j];
+                }
+                input[j + 1] = current;
+
+            }
+            Console.WriteLine("Sorted - {0}", string.Join(", ", input));
+        }
+    }
+}
